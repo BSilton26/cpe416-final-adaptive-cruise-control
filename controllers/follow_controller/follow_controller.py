79b316dba1128685a89e31d3d30d57886e362e95
Cameron McClure-Coleman <cjmcclur@calpoly.edu>
2024-03-15 13:53:47 -0700

    follow controller updates still need pid functions

--- controllers/follow_controller/follow_controller.py
+++ controllers/follow_controller/follow_controller.py
@@ -19,7 +19,11 @@ KP = 0.001  # Proportional control constant
 
 
 class Vehicle:
-    def __init__(self, kp=KP):
+    def __init__(self, angular_kp=ANGULAR_KP,
+                 velocity_kp = VELOCITY_KP,
+                 velocity_ki=VELOCITY_KI,
+                 velocity_kd=VELOCITY_KD,
+                 follow_distance=FOLLOW_DISTANCE):
         # Most of this stuff is copied from the provided my_controller file.
         # The majority don't even really NEED to be instance variables,
         # but I've made them so for convenience, just in case.
@@ -66,41 +70,50 @@ class Vehicle:
     def step(self):
         return self.robot.step(self.timestep)
 
-    def p_control(self):
+    def p_angular_control(self):
         """Modifies motor speeds according to the given error and proportional constant"""
-        speed_adjustment = self.kp * self.calc_ground_error() # todo: make adjustment proportional to current speed
+        speed_adjustment = self.angular_kp * self.calc_ground_error() # todo: make adjustment proportional to current speed
         self.left_motor.setVelocity(self.current_velocity - speed_adjustment)
         self.right_motor.setVelocity(self.current_velocity + speed_adjustment)
 
-    def p_velocity_control(self, distance):
-        return 0
+    def p_velocity_control(self, error) -> float:
+        """Modifies the target speed according to proportional constant"""
+        target_velocity_adjustment = self.velocity_kp * error
+        return round(target_velocity_adjustment)
 
-    def i_velocity_control(self, distance):
-        return 0
+    def i_velocity_control(self) -> float:
+        # sum the errors
+        accumulated_error = sum(self.past_errors)
+        return accumulated_error * self.velocity_ki
 
-    def d_velocity_control(self, distance):
-        return 0
+    def d_velocity_control(self) -> float:
+        return 0.0 # no derivative term; I don't wanna.
 
-    def pid_velocity_control(self, distance):
-        too_far = 20
+    def update_error(self, distance):
+        # calculate current error
+        if distance <= self.follow_distance:
+            current_error = distance - self.follow_distance
+        else:
+            current_error = 0
+        # add the current error to the recent error queue
+        if len(self.past_errors) >= 10:
+            self.past_errors = self.past_errors[:10]
+            self.past_errors.append(current_error)
+
+    def pid_velocity_correction(self):
+        too_far = self.follow_distance
         # collect distances
         front_left_distance = self.front_left_distance_sensor.getValue()
         front_right_distance = self.front_right_distance_sensor.getValue()
 
-        if front_left_distance <= too_far and front_right_distance <= too_far:
-            return self.target_velocity
-        elif front_left_distance > too_far:
+        if closest_distance <= too_far:
+            self.current_velocity = self.target_velocity
+        else:
             # something is in front of the robot. do pid logic to slow down in order to not hit it
-            pid_correction = self.p_velocity_control(front_left_distance) + self.i_velocity_control(front_left_distance) + self.d_velocity_control(front_left_distance)
-            return pid_correction
-        elif front_right_distance > too_far:
-            pid_correction = self.p_velocity_control(front_right_distance) + self.i_velocity_control(front_right_distance) + self.d_velocity_control(front_left_distance)
-            return pid_correction
-
-
-    def get_right_distance_reading(self) -> float:
-        """Returns the current reading from the right distance sensor"""
-        return self.right_distance_sensor.getValue()
+            pid_correction = (self.p_velocity_control(closest_distance) +
+                              self.i_velocity_control() +
+                              self.d_velocity_control())
+            self.current_velocity = pid_correction
 
     def get_ground_sensors(self) -> list:
         """Returns an array of the current ground sensor readings."""
@@ -152,130 +165,18 @@ class Vehicle:
         print(new_encoder_values)
         print('-------------------------')
 
-    def is_tower(self, position) -> bool:
-        """Returns True if the position is expected to be adjacent to a tower. False otherwise."""
-        assert 0 <= position < 360
-        res = False
-        for tower in self.tower_bounds:
-            res = res or tower[0] <= position <= tower[1]
-        return res
-
-    def resample_particles(self):
-        """Resamples the particles according to their current weights."""
-        new_particles = choices(
-            self.particles,
-            weights=[p.weight for p in self.particles],
-            k=self.num_particles - NUM_RANDOM_PARTICLES  # leave noise for random noise particles
-        )
-        for i in range(NUM_RANDOM_PARTICLES):
-            new_particles.append(Particle())
-        self.particles = new_particles
-        # Now make a new best guess as to current position (the statistical mode of all positions)
-        self.location = mode([p.position for p in self.particles])
-
-    def turn_to_target(self):
-        print('*krsh* take him down, over *krsh*')
-        self.left_motor.setVelocity(BASE_VELOCITY)
-        self.right_motor.setVelocity(-1 * BASE_VELOCITY)
-        target_time = self.robot.getTime() + 2.3  # takes about 2.3 seconds to turn 90 degrees
-        while self.robot.getTime() < target_time:
-            self.step()
-        self.right_motor.setVelocity(BASE_VELOCITY)
-
-
-def add_noise(mean=0, std_dev=1.0):
-    """Returns a value to use as Gaussian noise."""
-    rand1 = random()
-    rand2 = random()
-    z = sqrt(-2 * log(rand1)) * cos(2 * pi * rand2)
-    return mean + (std_dev * z)
-
-
-def trapezoid(a, b, c, d, x, maximum, minimum):
-    """a, b, c, d are distances from the sensor where the function changes slope
-    min and max should always be 0 & 1 because this function is being used to set probabilities
-       |
-    max|       -------
-       |      |        \\
-       |     |           \\
-    min|____|______________\\_______
-            a  b      c    d
-    """
-    if x < a:
-        res = 0.0001
-    elif a < x <= b:
-        res = (((x - a) / (b - a)) * (maximum - minimum)) + minimum
-    elif b < x <= c:
-        res = 1
-    elif c < x <= d:
-        res = (((x - c) / (d - c)) * (maximum - minimum)) + minimum
-    else:  # d < x
-        res = 0.0001
-    return res
-
-
-def trapezoid_free_space(dist):
-    """Wrapper function for free space trapezoid so that numbers can be more easily tuned."""
-    # a = 1.0
-    # b = 2.0
-    # c = 5.0
-    # d = 10.0
-    a = 0
-    b = 48
-    c = 80
-    d = 150
-    maximum = 1.0
-    minimum = 0.0
-    return trapezoid(a, b, c, d, dist, maximum, minimum)
-
-
-def trapezoid_tower(dist):
-    """Wrapper function for tower trapezoid so that numbers can be more easily tuned."""
-    # a = 7.0
-    # b = 12.0
-    # c = 15.0
-    # d = 17.0
-    a = 80
-    b = 120
-    c = 230
-    d = 300
-    maximum = 1.0
-    minimum = 0.0
-    return trapezoid(a, b, c, d, dist, maximum, minimum)
-
-
 # Main loop:
 # - perform simulation steps until Webots stops the controller
 def main():
     """Main function."""
-    towers = TOWERS
-    target = TARGET
-    vehicle = Vehicle(towers=towers, target=target)
-    target_tower_bounds = vehicle.tower_bounds[vehicle.target]
-    print(f'Target tower_bounds: {target_tower_bounds}')
+    vehicle = Vehicle()
     while vehicle.step() != -1:
         # Follow line
-        vehicle.p_control()
-        std_dev = 100
-        degrees_traveled = vehicle.update_position(dryrun=True)
+        vehicle.pid_velocity_correction()
+        vehicle.p_angular_control()
         # Update every 5 degrees traveled
-        if degrees_traveled > 5:
-            degrees_traveled = vehicle.update_position()
-            distance_reading = vehicle.get_right_distance_reading()
-            vehicle.update_particles(degrees_traveled, distance_reading)
-            vehicle.resample_particles()
-            # print(vehicle.particles)
-            print(f'{distance_reading=}')
-            print(f'{vehicle.location=}')
-            std_dev = stdev([p.position for p in vehicle.particles])
-            print(f'{std_dev=}')
         # If current position is at target and vehicle is reasonably certain of its position, attack
-        if (target_tower_bounds[0] < vehicle.location < target_tower_bounds[1] and
-                std_dev < STD_DEV_THRESHOLD):
-            print("BREAK BREAK BREAK")
-            break
     # break out of while loop when sure of positions and at target tower position
-    vehicle.turn_to_target()
 
 
 if __name__ == '__main__':
